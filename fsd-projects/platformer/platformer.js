5d31dc771a4ee193e9b7d0cf28e4173e62628d31
hotdogdude <adamstpierre11@gmail.com>
2026-09-03 17:19:47 +0000

    hi

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -35,30 +35,41 @@ createPlatform(500, 500, 20, 290);
 createPlatform(300, 620, 70, 20, "red");
 createBadPlatform(650, 570, 40, 600, "gray");
 createPlatform(000, 720, 1400, 100);
-createPlatform(850, 450, 60, 20);
-createPlatform(680, 320, 60, 20);
-createPlatform(970, 250, 60, 20, "red");
+createPlatform(850, 460, 60, 20);
+createPlatform(680, 340, 60, 20);
+createPlatform(950, 250, 60, 20, "red");
 createPlatform(1200, 000, 0, 250);
 createPlatform(1200, 400, 60, 400);
 createPlatform(1300, 300, 0, 20);
-createPlatform(520, 200, 60, 0);
+createPlatform(520, 210, 60, 1);
 createPlatform(1100, 620, 60, 20,"red");
 createPlatform(980, 550, 60, 20);
 createPlatform(720, 590, 60, 0);
-
+createBadPlatform(250, 400, 60, 20,"gray");
+createBadPlatform(150, 300, 60, 20,"gray");
+createBadPlatform(300, 200, 60, 20,"gray");
+createBadPlatform(200, 700, 60, 0,"gray");
+createPlatform(980, 550, 60, 20);
+createPlatform(400, 400, 60, 1);
     // TODO 3 - Create Collectables
 createCollectable("poo", 600, 400);
 createCollectable("poo", 315, 170, 0.5, 0.3);
 createCollectable("poo", 1300, 100);
-createCollectable("poo", 400, 100);
+createCollectable("poo", 420, 100);
 
     
     // TODO 4 - Create Cannons
 createCannon("top", 200, 540);
-createCannon("right", 300, 1100);
+createCannon("right", 300, 1800);
 createCannon("left", 550, 1100);
 createCannon("bottom", 550, 1100);
 createCannon("bottom", 1250, 1500);
+createCannon("left", 580, 3500);
+createCannon("left", 590, 3500);
+createCannon("left", 600, 3500);
+createCannon("left", 610, 3500);
+createCannon("left", 620, 3500);
+createCannon("top", 580, 2000);
     //////////////////////////////////
     // ONLY CHANGE ABOVE THIS POINT //
     //////////////////////////////////
